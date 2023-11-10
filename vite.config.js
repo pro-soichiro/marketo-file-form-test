d8dbcb103174e59337f09e4e11ca8270b906702a
pro-soichiro <dr.1987626@gmail.com>
2023-11-10 17:55:13 +0900

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  // base: '/marketo-file-form/',
+  base: '/marketo-file-form-test/',
   plugins: [vue()],
   resolve: {
     alias: {
